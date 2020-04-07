82897d3bf107d9aad28efde5635161caf25ee4ce
Heather Moreira <moreiraheather@gmail.com>
2020-04-07 20:22:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,11 +13,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\W$/)
-    true 
-  else 
-    false 
-  end 
+   text.match(/^[A-Z].*\W$/)? true: false 
+  #   true 
+  # else 
+  #   false 
+  # end 
 end
 
 def valid_phone_number?(phone)
