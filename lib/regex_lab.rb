a5459dcc1df8c1cac26210754420a3cf5a30329c
Heather Moreira <moreiraheather@gmail.com>
2020-04-07 20:34:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,6 +17,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
+  phone.scan(/\A[+-]?\d+(\.[\d]+)?\z/)
   binding.pry 
 end
